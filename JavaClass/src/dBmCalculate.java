1f78400e7825b4976c0bd3163bf29816e23e2b72
hellookjw <hellookjw@jiwookim>
2024-10-07 18:09:13 +0900

    1

--- JavaClass/src/dBmCalculate.java
+++ JavaClass/src/dBmCalculate.java
@@ -13,11 +13,8 @@ public class dBmCalculate {
 
         double power = volts * volts / resistance;
         System.out.println("전력은 " + power + "와트");
-        double mW = power * 1000;
-
-        double dBmFrommW = 10*Math.log10(mW);
+        
         double dBmFromW = 10*Math.log10(power) + 30;
-        System.out.println("dBm은: " + dBmFrommW);
         System.out.println("dBm은: " + dBmFromW);
 
         scanner.close();
