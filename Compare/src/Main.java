5fb45d159d0df99b556c1f2421c9f8908edb0c47
hellookjw <hellookjw@jiwookim>
2024-11-11 18:29:06 +0900

    ..

--- Compare/src/Main.java
+++ Compare/src/Main.java
@@ -1,30 +1,55 @@
 import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class Main {
-    public static Date[] createDateArray(String[] dateStrings) {
+    String[] names = { "CompareModifiedDate", "CompareFileType", "CompareFileName", "Main", "CompareSize"  };
+    String[] types = { "java", "java", "class", "java", "Class" };
+    int[] sizes = { 120, 80, 150, 85, 100 };
+    String[] dateStrings = { "2022-09-13T21:59:00", "2022-09-12T21:59:00",
+            "2022-09-13T16:54:00", "2022-09-12T21:54:00", "2022-09-13T11:59:00" };
+
+    public FileInfo[] createFileInfoArrays() {
+        FileInfo[] fileLists = new FileInfo[names.length];
         SimpleDateFormat dateTimeInstance = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-        Date[] dates = new Date[dateStrings.length];
-        for (int i = 0; i < dateStrings.length; i++) {
-            dates[i] = dateTimeInstance.parse(dateStrings[i], new ParsePosition(0));
+
+        for (int i = 0; i < fileLists.length; i++) {
+            Date date = dateTimeInstance.parse(dateStrings[i], new ParsePosition(0));
+            fileLists[i] = new FileInfo(names[i], types[i], sizes[i], date);
         }
-        return dates;
+        return fileLists;
+    }
+
+    public void printFileLists(FileInfo[] fileLists) {
+        for (FileInfo fi : fileLists) {
+            System.out.println(fi);
+        }
     }
 
     public static void main(String[] args) {
-        Integer[] data = { 1, 3, 7, 5, 6, 4, 10, 8 };
-        String[] data2 = { "hello", "hello1", "world1", "world", "new", "next" };
-        String[] dateStrings = { "2022-09-13T21:59:00", "2022-09-12T21:59:00",
-                "2022-09-13T16:54:00", "2022-09-12T21:54:00", "2022-09-13T11:59:00" };
+        Main m = new Main();
+        FileInfo[] fileLists = m.createFileInfoArrays();
+        System.out.println("원본 리스트");
+        m.printFileLists(fileLists);
 
-        Date[] dates = Main.createDateArray(dateStrings);
-        Sorter si = new Sorter();
-        si.bubbleSortInts(data);
-        Printer.print(data);
-        si.bubbleSortStrings(data2);
-        Printer.print(data2);
-        si.bubbleSortDates(dates);
-        Printer.print(dates);
+        Sorter sorter = new Sorter(new CompareFileName());
+        System.out.println("\n\n파일 이름으로 정렬된 리스트");
+        sorter.bubbleSort(fileLists);
+        m.printFileLists(fileLists);
+
+        System.out.println("\n\n파일 종류로 정렬된 리스트");
+        sorter.setComparable(new CompareFileType());
+        sorter.bubbleSort(fileLists);
+        m.printFileLists(fileLists);
+
+        System.out.println("\n\n파일 크기로 정렬된 리스트");
+        sorter.setComparable(new CompareFileSize());
+        sorter.bubbleSort(fileLists);
+        m.printFileLists(fileLists);
+
+        System.out.println("\n\n파일 수정 시간으로 정렬된 리스트");
+        sorter.setComparable(new CompareModifiedDate());
+        sorter.bubbleSort(fileLists);
+        m.printFileLists(fileLists);
     }
 }
